5f60fd5c3c1211270c3d456738c6f88906e8298f
eunwoo-levi <eunwoo1341@gmail.com>
2026-01-26 18:20:08 +0900

    feat: 상품 상세 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProductsPage } from "@/pages/products/ProductsPage";
+import { ProductDetailPage } from "@/pages/products/ProductDetailPage";
 import "./App.css";
 
 function App() {
-  return <ProductsPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
